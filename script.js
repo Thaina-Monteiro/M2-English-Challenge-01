60cb60bd462bcb617e6c9193778fe1e5cf17bc99
Thainá Monteiro <96400582+Thaina-Monteiro@users.noreply.github.com>
2022-06-28 22:23:55 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+
+var comedy = [{
+    title: 'White Chicks',
+    director: 'Keenen Ivory Wayans',
+    year: 2004
+},
+{
+    title: 'This Means War',
+    director: 'McG',
+    year: 2012
+},
+{
+    title: 'The Layover',
+    director: ' William H. Macy',
+    year: 2017
+}
+]
+var action = [{
+    title: 'The Batman',
+    director: 'Matt Reeves',
+    year: 2022
+},
+{
+    title: 'Interceptor',
+    director: 'Matthew Reilly',
+    year: 2022
+},
+{
+    title: 'Matrix Resurrections',
+    director: 'Lana Wachowski',
+    year: 2021
+}
+]
+var fantasy = [{
+    title: 'Fantastic Beasts: The Crimes of Grindelwald.',
+    director: 'David Yates',
+    year: 2018
+},
+{
+    title: 'Encanto',
+    director: 'Jared Bush',
+    year: 2021
+},{
+    title : 'Rise of the Guardians',
+    director : 'Peter Ramsey',
+    year : 2012
+}
+]
+
+if(action[0].title === 'The Batman' ){
+    console.log(`My favorite movie is ${action[0].title}`)
+}else{
+    console.log('Not even 5 stars...')
+}
+
+
